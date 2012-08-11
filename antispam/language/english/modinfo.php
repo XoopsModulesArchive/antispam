559bfe3d27bcc842b92eea8a97fa3a4bce77c1ca
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- antispam/language/english/modinfo.php
+++ antispam/language/english/modinfo.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * $Id: modinfo.php,v 1.7 2008/10/13 11:52:53 xoops Exp $
  * Module: badbehavior
@@ -11,30 +11,30 @@
 // Module Info
 // The name of this module
 global $xoopsModule;
-define("_MI_BB2_MD_NAME", "Antispam");
+define("_MI_BB2_MD_NAME","Antispam");
 
 // A brief description of this module
-define("_MI_BB2_MD_DESC", "spam protection module");
+define("_MI_BB2_MD_DESC","spam protection module");
 
 // module options
 $cf=1;
-define("_MI_BB2_STRICT_MODE", "$cf. Enable strict mode ?");
-define("_MI_BB2_STRICT_MODEDSC", "Enable strict checking (blocks more spam but may block some people)");
+define("_MI_BB2_STRICT_MODE","$cf. Enable strict mode ?");
+define("_MI_BB2_STRICT_MODEDSC","Enable strict checking (blocks more spam but may block some people)");
 $cf++;
 define("_MI_BB2_VERBOSE","$cf. Enable verbose logging ?"); 
 define("_MI_BB2_VERBOSEDSC","Enables or disables verbose logging which includes all requests, not just failed ones"); 
 $cf++;
-define("_MI_BB2_ALERTMAIL", "$cf. Webmaster-Mail");
-define("_MI_BB2_ALERTMAILDSC", "Administrator email address for blocked users to contact to gain access");
+define("_MI_BB2_ALERTMAIL","$cf. Webmaster-Mail");
+define("_MI_BB2_ALERTMAILDSC","Administrator email address for blocked users to contact to gain access");
 $cf++;
-define("_MI_BB2_HTTPBLKEY", "$cf. Optionally enter your Honeypot-ID here");
-define("_MI_BB2_HTTPBLKEYDSC", "If you want to use HTTP:BL with BadBehavior, get a HTTP:BL key from Project HoneyPot");
+define("_MI_BB2_HTTPBLKEY","$cf. Optionally enter your Honeypot-ID here");
+define("_MI_BB2_HTTPBLKEYDSC","If you want to use HTTP:BL with BadBehavior, get a HTTP:BL key from Project HoneyPot");
 
 // Names of admin menu items
-define("_MI_BB2_ADMENU0", "Index");
-define("_MI_BB2_ADMENU1", "Statistik");
+define("_MI_BB2_ADMENU0","Index");
+define("_MI_BB2_ADMENU1","Statistik");
 // Names of admin header menu items
-define('_MI_BB2_HDMENU0', 'refresh Modul Templates');
-define("_MI_BB2_HDMENU1", "About");
+define('_MI_BB2_HDMENU0','refresh Modul Templates');
+define("_MI_BB2_HDMENU1","About");
 
 ?>
