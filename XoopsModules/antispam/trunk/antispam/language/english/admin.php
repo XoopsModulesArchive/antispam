d2b2ab4bd972f119f2231443a0f1d3995f2f4573
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- XoopsModules/antispam/trunk/antispam/language/english/admin.php
+++ XoopsModules/antispam/trunk/antispam/language/english/admin.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * $Id: admin.php,v 1.7 2008/10/13 11:52:53 xoops Exp $
  * Module: badbehavior
@@ -6,51 +6,51 @@
  * Author: yerres
  * Licence: GNU
  */
-define("_AM_BB2_ACTION", "Action");
-define("_AM_BB2_ID", "ID");
-define("_AM_BB2_IP", "IP");
-define("_AM_BB2_DATE", "Date");
-define("_AM_BB2_REQUEST_METHOD", "Request Type");
-define("_AM_BB2_REQUEST_URI", "URI");
-define("_AM_BB2_SERVER_PROTOCOL", "Protocol");
-define("_AM_BB2_HTTP_HEADERS", "HTTP Headers");
-define("_AM_BB2_USER_AGENT", "User Agent");
-define("_AM_BB2_REQUEST_ENTITY", "Request Entity");
-define("_AM_BB2_KEY", "Response Key");
-define("_AM_BB2_SHOWRECS", "Log records");
-define("_AM_BB2_NORECS", "No log messages available");
-define("_AM_BB2_INVENTORY", "Log Summary");
-define("_AM_BB2_DETAILS", "Details");
-define("_AM_BB2_REASON", "Denied Reason");
-define("_AM_BB2_EXP", "Explanation");
-define("_AM_BB2_HOST", "Hostname");
+define("_AM_BB2_ACTION","Action");
+define("_AM_BB2_ID","ID");
+define("_AM_BB2_IP","IP");
+define("_AM_BB2_DATE","Date");
+define("_AM_BB2_REQUEST_METHOD","Request Type");
+define("_AM_BB2_REQUEST_URI","URI");
+define("_AM_BB2_SERVER_PROTOCOL","Protocol");
+define("_AM_BB2_HTTP_HEADERS","HTTP Headers");
+define("_AM_BB2_USER_AGENT","User Agent");
+define("_AM_BB2_REQUEST_ENTITY","Request Entity");
+define("_AM_BB2_KEY","Response Key");
+define("_AM_BB2_SHOWRECS","Log records");
+define("_AM_BB2_NORECS","No log messages available");
+define("_AM_BB2_INVENTORY","Log Summary");
+define("_AM_BB2_DETAILS","Details");
+define("_AM_BB2_REASON","Denied Reason");
+define("_AM_BB2_EXP","Explanation");
+define("_AM_BB2_HOST","Hostname");
 
 // about
-define("_AM_BB2_ABOUT_RELEASEDATE", "Release date");
-define("_AM_BB2_ABOUT_AUTHOR", "Author");
-define("_AM_BB2_ABOUT_CREDITS", "Credits");
-define("_AM_BB2_ABOUT_LICENSE", "License");
-define("_AM_BB2_ABOUT_WEBSITE", "Homepage");
-define("_AM_BB2_ABOUT_MODULE_INFO", "Module info");
-define("_AM_BB2_ABOUT_MODULE_STATUS", "Status");
-define("_AM_BB2_ABOUT_MODULE_TEAM", "Team members");
-define("_AM_BB2_ABOUT_AUTHOR_INFO", "Author info");
-define("_AM_BB2_ABOUT_AUTHOR_NAME", "Author name");
-define("_AM_BB2_ABOUT_AUTHOR_WORD", "Author's word");
-define("_AM_BB2_ABOUT_DISCLAIMER", "Disclaimer");
-define("_AM_BB2_ABOUT_DISCLAIMER_TEXT", "GPL-licensed.<BR>This module comes as is, without any guarantees whatsoever. This module is a Release Candidate and should not be used on a production web site. The module is still under active development and its use is under your own responsibility, which means the author is not responsible.");
-define("_AM_BB2_ABOUT_DISCLAIMER_TEXT_1", "Authors word:");
-define("_AM_BB2_ABOUT_CHANGELOG", "Changelog");
-define("_AM_BB2_ABOUT_README", "Readme");
-define("_AM_BB2_ABOUT_DEMO", "Demo Site(s)");
-define("_AM_BB2_ABOUT_SUBMIT_BUG", "Report a bug for this module");
-define("_AM_BB2_ABOUT_SUBMIT_BUG_TEXT", "Submit a Bug");
-define("_AM_BB2_ABOUT_SUBMIT_FEATURE", "Suggest a new feature for this module");
-define("_AM_BB2_ABOUT_SUBMIT_FEATURE_TEXT", "Submit a feature request");
-define("_AM_BB2_ABOUT_DESCRIPTION", "Description");
-define("_AM_BB2_ABOUT_UPDATEDATE", "Update");
+define("_AM_BB2_ABOUT_RELEASEDATE","Release date");
+define("_AM_BB2_ABOUT_AUTHOR","Author");
+define("_AM_BB2_ABOUT_CREDITS","Credits");
+define("_AM_BB2_ABOUT_LICENSE","License");
+define("_AM_BB2_ABOUT_WEBSITE","Homepage");
+define("_AM_BB2_ABOUT_MODULE_INFO","Module info");
+define("_AM_BB2_ABOUT_MODULE_STATUS","Status");
+define("_AM_BB2_ABOUT_MODULE_TEAM","Team members");
+define("_AM_BB2_ABOUT_AUTHOR_INFO","Author info");
+define("_AM_BB2_ABOUT_AUTHOR_NAME","Author name");
+define("_AM_BB2_ABOUT_AUTHOR_WORD","Author's word");
+define("_AM_BB2_ABOUT_DISCLAIMER","Disclaimer");
+define("_AM_BB2_ABOUT_DISCLAIMER_TEXT","GPL-licensed.<BR>This module comes as is, without any guarantees whatsoever. This module is a Release Candidate and should not be used on a production web site. The module is still under active development and its use is under your own responsibility, which means the author is not responsible.");
+define("_AM_BB2_ABOUT_DISCLAIMER_TEXT_1","Authors word:");
+define("_AM_BB2_ABOUT_CHANGELOG","Changelog");
+define("_AM_BB2_ABOUT_README","Readme");
+define("_AM_BB2_ABOUT_DEMO","Demo Site(s)");
+define("_AM_BB2_ABOUT_SUBMIT_BUG","Report a bug for this module");
+define("_AM_BB2_ABOUT_SUBMIT_BUG_TEXT","Submit a Bug");
+define("_AM_BB2_ABOUT_SUBMIT_FEATURE","Suggest a new feature for this module");
+define("_AM_BB2_ABOUT_SUBMIT_FEATURE_TEXT","Submit a feature request");
+define("_AM_BB2_ABOUT_DESCRIPTION","Description");
+define("_AM_BB2_ABOUT_UPDATEDATE","Update");
 // index
-define("_AM_BB2_CONFIG_CHECK", "Configuration Check");
-define("_AM_BB2_CONFIG_PHP", "Minimum PHP Version");
-define("_AM_BB2_CONFIG_XOOPS", "Minimum XOOPS Version");
+define("_AM_BB2_CONFIG_CHECK","Configuration Check");
+define("_AM_BB2_CONFIG_PHP","Minimum PHP Version");
+define("_AM_BB2_CONFIG_XOOPS","Minimum XOOPS Version");
 ?>
